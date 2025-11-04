86030d9a0c6f51c1f7720fea0f37a74c7836da6d
Stu Finn <stu@borealisweb.ca>
2025-11-04 13:48:38 -0500

    Initial setup for reun_python_file

---
+++ functions/run_python_file.py
@@ -0,0 +1,13 @@
+import os
+
+def run_python_file(working_directory, file_path, args=[]):
+    abs_wd_path = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not abs_file_path.startswith(abs_wd_path):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(abs_file_path):
+        return f'Error: File "{file_path}" not found.'
+    if not file_path.endswith('.py'):
+        return f'Error: "{file_path}" is not a Python file.'
+    
--- tests.py
+++ tests.py
@@ -1,19 +1,24 @@
 # from functions.get_files_info import get_files_info
 # from functions.get_file_content import get_file_content
-from functions.write_file import write_file
+# from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 def test():
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    result = run_python_file("calculator", "lorem.txt")
     print(result)
 
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-    print(result)
+# def test():
+#     result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+#     print(result)
 
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-    print(result)
+#     result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+#     print(result)
 
-    result = write_file("calculator", "pkg", "this should not be allowed")
-    print(result)
+#     result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+#     print(result)
+
+#     result = write_file("calculator", "pkg", "this should not be allowed")
+#     print(result)
 
 
 # def test():
