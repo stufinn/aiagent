2b0adebc921efa11d3f1fe7832e6f3bb95bacfdb
Stu Finn <stu@borealisweb.ca>
2025-11-04 12:32:54 -0500

    Add file content retrieval functionality and tests; create config for max character limit

---
+++ functions/get_file_content.py
@@ -0,0 +1,27 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    wd_path = os.path.abspath(working_directory)
+    target_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not target_path.startswith(wd_path):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(target_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+
+    try:
+
+        file_size = os.path.getsize(target_path)
+
+        with open(target_path, "r") as file:
+            file_content_string = file.read(MAX_CHARS)
+
+            if file_size > MAX_CHARS:
+                file_content_string += f'[...File {file_path} truncated at {MAX_CHARS} characters]'
+
+            return file_content_string
+        
+    except Exception as e:
+        return(f"Error reading file content: {e}")
--- tests.py
+++ tests.py
@@ -1,24 +1,41 @@
-from functions.get_files_info import get_files_info
-
-
+# from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 def test():
-    result = get_files_info("calculator", ".")
-    print("Result for current directory:")
-    print(result)
-    print("")
-
-    result = get_files_info("calculator", "pkg")
-    print("Result for 'pkg' directory:")
+    result = get_file_content("calculator", "main.py")
     print(result)
 
-    result = get_files_info("calculator", "/bin")
-    print("Result for '/bin':")
+    result = get_file_content("calculator", "pkg/calculator.py")
     print(result)
 
-    result = get_files_info("calculator", "../")
-    print("Result for '../':")
+    result = get_file_content("calculator", "/bin/cat")
     print(result)
+
+    result = get_file_content("calculator", "pkg/does_not_exist.py")
+    print(result)
+
+    # result = get_file_content("calculator", "lorem.txt")
+    # print(result)
+
+
+
+# def test():
+#     result = get_files_info("calculator", ".")
+#     print("Result for current directory:")
+#     print(result)
+#     print("")
+
+#     result = get_files_info("calculator", "pkg")
+#     print("Result for 'pkg' directory:")
+#     print(result)
+
+#     result = get_files_info("calculator", "/bin")
+#     print("Result for '/bin':")
+#     print(result)
+
+#     result = get_files_info("calculator", "../")
+#     print("Result for '../':")
+#     print(result)
 
 
 if __name__ == "__main__":
