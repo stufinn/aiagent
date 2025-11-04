490e202acb9877fcda9c1a3094dbb74e53b7cf08
Stu Finn <stu@borealisweb.ca>
2025-11-04 11:49:28 -0500

    Refactor get_files_info function for improved error handling and clarity; update tests to encapsulate functionality in a test function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,46 +2,25 @@ import os
 
 def get_files_info(working_directory, directory="."):
 
-    try:
+    abs_working_dir = os.path.abspath(working_directory)
+    target_dir = os.path.abspath(os.path.join(working_directory, directory))
 
-        results = f"Result for '{directory}' directory:"
-        new_directory = directory
-        # list contents of the working directory
-        wd_contents = os.listdir(working_directory)
-        
-        # if the directory is not in the working directory...
-        if directory not in wd_contents and directory != ".":
-            error = f'    Error: Cannot list "{directory}" as it is outside the permitted working directory'
-            results = results + "\n" + error
-            print(results)
-            return error
-        
-        # if no directory is provided, the working directory is the full path
-        if directory == ".":
-            new_directory = ""
-
-        full_path = os.path.join(working_directory, new_directory)
-        abs_full_path = os.path.abspath(full_path)
-        is_valid_directory = os.path.isdir(abs_full_path)
-
-        if not is_valid_directory:
-            error = f'    Error: "{directory}" is not a directory'
-            results += "\n" + error
-            print(results)
-            return error
-
-        # if we made it this far, then we are working with a directory
-        current_dir_contents = os.listdir(abs_full_path)
-
-        filtered_contents = [item for item in current_dir_contents if item != "__pycache__"]
-
-        
-        for item in filtered_contents:
-            path_to_item = os.path.join(abs_full_path, item)
-            results += "\n" + f" - {item}: file_size:{os.path.getsize(path_to_item)} bytes, is_dir={os.path.isdir(path_to_item)}"
-            
-        print(results)    
-        return results
+    if not target_dir.startswith(abs_working_dir):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     
+    if not os.path.isdir(target_dir):
+        f'Error: "{directory}" is not a directory'
+
+    try:
+        files_info = []
+        for filename in os.listdir(target_dir):
+            filepath = os.path.join(target_dir, filename)
+            file_size = 0
+            is_dir = os.path.isdir(filepath)
+            file_size = os.path.getsize(filepath)
+            files_info.append(
+                f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
+            )
+        return "\n".join(files_info)
     except Exception as e:
-        print(f"Error: {e}")
+        return f"Error listing files: {e}"
